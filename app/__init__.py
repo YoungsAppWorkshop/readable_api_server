0dd4facaf5207cb1109d8dd226147182cc2e3cf3
Young Jung <ryviusm6@gmail.com>
2018-02-26 18:17:07 +0900

    feat: Allow CORS using flask_cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Load default config
 app.config.from_object('config')
